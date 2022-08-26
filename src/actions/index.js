58576fb76f9ea11ccee01fad82a2d65bd048e314
bstewart3 <505propz@gmail.com>
2022-08-26 13:19:48 -0700

    added local storage to persist userId on refresh
    wired up buisnessList component
    
    refactored createBuisness action to send the proper response payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-import { SIGN_IN, SIGN_OUT, IS_SIGNEDIN,IS_NOT_SIGNEDIN } from "./types";
+import firebase from '../apis/firebase'
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_BUISNESS,
+    FETCH_BUISNESS,
+    FETCH_BUISNESSES,
+    EDIT_BUISNESS, 
+    DELETE_BUISNESS
+    } from "./types";
 
 export const signIn = (userId) => {
     return {
@@ -9,75 +17,46 @@ export const signIn = (userId) => {
     };
 };
 
+
 export const signOut = () => {
     return {
         type: SIGN_OUT
     };
 };
-export const isSignedIn = () => {
-    return {
-        type: IS_SIGNEDIN
+
+
+export const createBuisness = formValues => async (dispatch) => {
+        const response = await firebase.post('/buisnesses.json', formValues);
+        
+        dispatch({ type: CREATE_BUISNESS, payload: JSON.parse(response.config.data)})
     }
-} 
-export const isNotSignedIn = () => {
-    return {
-        type: IS_NOT_SIGNEDIN
-    }
-} 
 
-// export const createBuisness = async (formValues) =>  {
-    
-    
-//     const response = await fetch(`https://buisnesspad-default-rtdb.firebaseio.com/users/${userId}.json`, {
-//         method: 'POST',
-//         body: JSON.stringify(formValues)
-//     })
-//     if (!response.ok) {
-//         const error = new Error(response.message || 'Failed to post');
-//         throw error;
-//     }
 
-    
-// };
+export const fetchBuisness = (id) => async dispatch => {
+        const response = await firebase.get(`/buisnesses/${id}.json`)
 
-// export const createBuisness = createAsyncThunk('CREATE_BUISNESS', async (formValues) => {
-//     const response = await fetch(`https://buisnesspad-default-rtdb.firebaseio.com/users/.json`, {
-//         method: 'POST',
-//         body: JSON.stringify(formValues)
-//     })
-//     if (!response.ok) {
-//         const error = new Error(response.message || 'Failed to post');
-//         throw error;
-//     }
-
-    
-//     return {
-//         type: 'CREATE_BUISNESS',
-//         payload: JSON.stringify(response)
-//     };
-// })
-
-export const fetchBuisness = () => {
-    return {
-        type: 'FETCH_BUISNESS'
+        dispatch ({ type: FETCH_BUISNESS, payload: response.data})
     };
-};
-export const fetchBuisnesses = () => {
-    return {
-        type: 'FETCH_BUISNESSES'
-    };
+
+
+export const fetchBuisnesses = () => async dispatch => {
+    const response = await firebase.get('/buisnesses.json');
+
+    dispatch({ type: FETCH_BUISNESSES, payload: response.data})
 };
 
 
-export const editBuissness = () => {
-    return {
-        type: 'EDIT_BUISNESS'
-    };
+export const editBuissness = (id, formValues) => async dispatch =>  {
+    const response = await firebase.put(`/buisnesses/${id}`, formValues);
+
+    dispatch({ type: EDIT_BUISNESS, payload: response.data})
 };
-export const deleteBuisnes = () => {
-    return {
-        type: 'DELETE_BUISNESS'
-    };
+
+
+export const deleteBuisnes = (id) => async dispatch => {
+    await firebase.delete(`buisnesses/${id}`)
+
+    dispatch({ type: DELETE_BUISNESS, payload: id})
 };
 
 
